bf1514f29770c1eba55e6a8bad71c80e2215987c
Timothy Barland <tbarland77@hotmail.com>
2018-12-15 11:45:43 -0500

    added tmeplate tests for the card view component

--- src/app/components/card-view/card-view.component.spec.ts
+++ src/app/components/card-view/card-view.component.spec.ts
@@ -5,41 +5,42 @@ import { CardViewComponent } from './card-view.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { KanjiStudyService } from 'src/app/services/kanji-study.service';
+import { By } from '@angular/platform-browser';
 
 describe('CardViewComponent', () => {
   let component: CardViewComponent;
   let fixture: ComponentFixture<CardViewComponent>;
   const mockKanjiResponse = [
 
-      {
-        'category': 'jlptn5',
-        'character': '分',
-        'kunyomi': ' わ.ける、 わ.け、 わ.かれる、 わ.かる、 わ.かつ',
-        'meaning': 'day, sun, Japan',
-        'onyomi': 'ブン フン ブ'
-      },
-      {
-        'category': 'jlptn5',
-        'character': '八',
-        'kunyomi': 'や や.つ やっ.つ',
-        'meaning': 'eight',
-        'onyomi': 'ハチ ヨ'
-      },
-      {
-        'category': 'jlptn5',
-        'character': '気',
-        'kunyomi': 'いき',
-        'meaning': 'spirit, mind',
-        'onyomi': 'キ ケ'
-      },
-      {
-        'category': 'jlptn5',
-        'character': '子',
-        'kunyomi': 'こ -こ',
-        'meaning': 'child, sign of the rat, 11PM-1AM, first sign of Chinese zodiac',
-        'onyomi': 'シ ス ツ'
-      }
-    ];
+    {
+      'category': 'jlptn5',
+      'character': '分',
+      'kunyomi': ' わ.ける、 わ.け、 わ.かれる、 わ.かる、 わ.かつ',
+      'meaning': 'day, sun, Japan',
+      'onyomi': 'ブン フン ブ'
+    },
+    {
+      'category': 'jlptn5',
+      'character': '八',
+      'kunyomi': 'や や.つ やっ.つ',
+      'meaning': 'eight',
+      'onyomi': 'ハチ ヨ'
+    },
+    {
+      'category': 'jlptn5',
+      'character': '気',
+      'kunyomi': 'いき',
+      'meaning': 'spirit, mind',
+      'onyomi': 'キ ケ'
+    },
+    {
+      'category': 'jlptn5',
+      'character': '子',
+      'kunyomi': 'こ -こ',
+      'meaning': 'child, sign of the rat, 11PM-1AM, first sign of Chinese zodiac',
+      'onyomi': 'シ ス ツ'
+    }
+  ];
 
   class MockKanjiStudyService {
     getKanji() {
@@ -49,16 +50,16 @@ describe('CardViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CardViewComponent ],
+      declarations: [CardViewComponent],
       providers: [
-        { provide: ActivatedRoute,  useValue: {params: of({id: 'JLPTN5'})} },
+        { provide: ActivatedRoute, useValue: { params: of({ id: 'JLPTN5' }) } },
         { provide: KanjiStudyService, useClass: MockKanjiStudyService },
       ],
       schemas: [
         NO_ERRORS_SCHEMA
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -133,6 +134,37 @@ describe('CardViewComponent', () => {
       component.answer = mockKanjiResponse[1];
       component.compareUserSelectedAnswer(mockKanjiResponse[3]);
       expect(component.shuffleCards).toHaveBeenCalled();
+    });
+  });
+
+  describe('card view template tests', () => {
+    beforeEach(() => {
+      component.studyList = mockKanjiResponse;
+      component.answerIndex = 1;
+      fixture.detectChanges();
+    });
+    it('should display the character answer', () => {
+      const answerText = fixture.debugElement.query(By.css('.answer-character'));
+      expect(answerText.nativeElement.innerHTML)
+        .toBe(` Character: ${component.studyList[component.answerIndex].character} `);
+    });
+    it('should display the kunyomi for the current card', () => {
+      const answerText = fixture.debugElement.queryAll(By.css('.kunyomi'));
+      answerText.forEach((text, index) => {
+        expect(text.nativeElement.innerHTML).toBe(`訓読み: ${component.studyList[index].kunyomi}`);
+      });
+    });
+    it('should display the onyomi for the current card', () => {
+      const answerText = fixture.debugElement.queryAll(By.css('.onyomi'));
+      answerText.forEach((text, index) => {
+        expect(text.nativeElement.innerHTML).toBe(`音読み: ${component.studyList[index].onyomi}`);
+      });
+    });
+    it('should display the english meaning for the current card', () => {
+      const answerText = fixture.debugElement.queryAll(By.css('.meaning'));
+      answerText.forEach((text, index) => {
+        expect(text.nativeElement.innerHTML).toBe(`意味(英語): ${component.studyList[index].meaning}`);
+      });
     });
   });
 });
